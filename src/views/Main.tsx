a6121ef82ea103a66302db79ddbde5453fbafd38
tinyking <akingchao@qq.com>
2021-05-28 13:48:23 +0800

    1

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,6 +1,8 @@
 import { Layout } from 'antd';
 import RockHeader from '../components/RockHeader';
 import Dashboard from './Dashboard';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -13,7 +15,13 @@ const Main = () => (
       </Header>
       <Content>
           <div className="main">
-             <Dashboard />
+             <Router>
+                 <Suspense fallback={<div>Loading...</div>}>
+                     <Switch>
+                         <Route exact path="/" component={Dashboard}></Route>
+                     </Switch>
+                 </Suspense>
+             </Router>
           </div>
       </Content>
       <Footer>Footer</Footer>
